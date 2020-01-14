704c8470ea911ddcaa790108df6848307e6bc3e6
zixun <lvzixun@gmail.com>
2020-01-14 20:38:27 +0800

    update testcase

--- test_zxml.lua
+++ test_zxml.lua
@@ -1,7 +1,7 @@
 local zxml_c = require "zxml.core"
 local print_r =require "print_r"
 local zxml_parser_excel_xml2003 = zxml_c.zxml_parser_excel_xml2003
-
+local zxml_parser = zxml_c.zxml_parser
 
 local function readfile(path)
     local fd = io.open(path, "r")
@@ -12,6 +12,8 @@ end
 
 local xml_path = ...
 local xml_source = readfile(xml_path)
-local t  = zxml_parser_excel_xml2003(xml_source)
+local t1 = zxml_parser(xml_source)
+local t2  = zxml_parser_excel_xml2003(xml_source)
+print_r(t1)
 print("-----------------")
-print_r(t)
+print_r(t2)
